f0fac2e36d83c6bc7f54aec489e009d2fdcbd537
Administrator <sreddouilyongxia@qq.com>
2018-07-01 13:38:25 +0800

    新标签页中打开新标签页

--- src/pages/demo/index.jsx
+++ src/pages/demo/index.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import {Button, Table, pagination}  from 'antd/lib';
 
 class Demo extends Component {
+    constructor(props){
+        super(props);
+    }
     render () {
         return (
             <div className="demo">
                 <h1>决策流配置</h1>
                 This is a React Demo example.
+                <Button type="primary" onClick={this.props.opennewTab.bind(this, {name:"newTabView",id:"newTabView"})}>新建Tab页</Button>
             </div>
         )
     }
